e4fb468a3a42f02e69946e54a868ccf76b2b0664
Luis Hau <ycluisdev@gmail.com>
2023-08-05 00:38:31 +0800

    Cleanup folder structure, setup skeleton draft to interact with openWeatherAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import InputForm from './components/InputForm'
+import Weather from './components/Weather'
+
+import openWeatherService from './services/weatherService'
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [weather, setWeather] = useState({})
+
+  const getWeatherData = async (city) => {
+    const locationGeo = await openWeatherService.getLocationGeo(city)
+    const weatherData = await openWeatherService.getWeather(locationGeo[0].lat, locationGeo[0].lon)
+
+    console.log(weatherData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputForm getWeatherData={getWeatherData} />
+      <Weather />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

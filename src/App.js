5a91c2dd1b0185992204dc75d2aeb52b09ca525f
Luis Hau <ycluisdev@gmail.com>
2023-08-05 10:51:53 +0800

    Update weather component to display raw data return from API

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import openWeatherService from './services/weatherService'
 
 function App() {
   // eslint-disable-next-line no-unused-vars
+  const [geo, setGeo] = useState({})
   const [weather, setWeather] = useState({})
 
   const getWeatherData = async (city) => {
     const locationGeo = await openWeatherService.getLocationGeo(city)
     const weatherData = await openWeatherService.getWeather(locationGeo[0].lat, locationGeo[0].lon)
 
-    console.log(weatherData)
+    setGeo(locationGeo[0])
+    setWeather(weatherData)
   }
 
   return (
     <div>
       <InputForm getWeatherData={getWeatherData} />
-      <Weather />
+      <Weather geo={geo} weather={weather} />
     </div>
   )
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,43 @@
-const Weather = () => {
-  return <div>Weather Data</div>
+const Weather = ({ geo, weather }) => {
+  return (
+    <>
+      {Object.keys(weather).length !== 0 && Object.keys(geo).length !== 0 && (
+        <>
+          <h2>WeatherData</h2>
+          <h3>
+            {geo.name}, {geo.country}
+          </h3>
+          <h3>{weather.weather[0].main}</h3>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="icon" />
+          <p>{weather.weather[0].description}</p>
+          <p>
+            {`${(weather.main.temp_min - 273.15).toFixed(2)}\u00B0C`} ~{' '}
+            {`${(weather.main.temp_max - 273.15).toFixed(2)}\u00B0C`}
+          </p>
+          <p>{weather.main.humidity}%</p>
+          <p>
+            {new Date(1691201759 * 1000)
+              .toLocaleString([], {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              })
+              .replaceAll('/', '-')
+              .replaceAll(',', '')}
+          </p>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Weather
+
+// weather[0].main
+// weather[0].description
+// main.tempmin & main.tempmax
+// main.huminity
+// dt

b004ffeec98e5d38752b7bec30ca67239e10e78b
Luis Hau <ycluisdev@gmail.com>
2023-08-05 16:27:05 +0800

    Impl UI for input & weather components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
 import InputForm from './components/InputForm'
@@ -5,6 +6,18 @@ import Weather from './components/Weather'
 import History from './components/History'
 
 import openWeatherService from './services/weatherService'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+
+// MUI dark theme
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 function App() {
   const [geo, setGeo] = useState({})
@@ -32,11 +45,23 @@ function App() {
   }
 
   return (
-    <div>
-      <InputForm onGetWeatherData={getWeatherData} />
-      <Weather geo={geo} weather={weather} />
-      <History history={history} onGetWeatherData={getWeatherData} onItemDelete={handleHistoryItemDelete} />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container fixed style={{ display: 'flex', flexDirection: 'column' }}>
+        <Box p={4} style={{ margin: '1rem', flex: 1 }}>
+          <InputForm onGetWeatherData={getWeatherData} />
+        </Box>
+        {history.length > 0 && (
+          <Box
+            p={4}
+            style={{ margin: '1rem', backgroundColor: 'rgba(40, 18, 77, 0.5)', flex: 1, borderRadius: '20px' }}
+          >
+            <Weather geo={geo} weather={weather} />
+            {/* <History history={history} onGetWeatherData={getWeatherData} onItemDelete={handleHistoryItemDelete} /> */}
+          </Box>
+        )}
+      </Container>
+    </ThemeProvider>
   )
 }
 
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,19 +1,62 @@
+import { useState } from 'react'
+import { TextField, Button, Paper } from '@mui/material'
+import Box from '@mui/material/Box'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+
 const InputForm = ({ onGetWeatherData }) => {
-  const getWeather = (e) => {
-    e.preventDefault()
-    onGetWeatherData(e.target.city.value)
-    e.target.city.value = ''
+  const [city, setCity] = useState('')
+
+  const getWeather = () => {
+    if (city) {
+      onGetWeatherData(city)
+      setCity('')
+    }
   }
 
   return (
     <>
-      <h2>Today Weather</h2>
-      <form onSubmit={getWeather}>
-        <div>
-          <input name="city" />
-        </div>
-        <button>submit</button>
-      </form>
+      <Box display={'flex'}>
+        <Paper
+          elevation={3}
+          sx={{
+            flexGrow: 1,
+            backgroundColor: 'rgba(40, 18, 77, 1)',
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: '20px',
+            marginRight: '1rem',
+          }}
+        >
+          <TextField
+            label="City"
+            variant="outlined"
+            type="text"
+            name="city"
+            value={city}
+            fullWidth
+            InputProps={{
+              sx: {
+                color: '#ffffff',
+                borderRadius: '20px',
+              },
+            }}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </Paper>
+
+        <Button
+          startIcon={<SearchOutlinedIcon />}
+          onClick={getWeather}
+          sx={{
+            px: 2,
+            backgroundColor: 'rgba(40, 18, 77, 1)',
+            color: '#ffffff',
+            borderRadius: '20px',
+          }}
+        >
+          Search
+        </Button>
+      </Box>
     </>
   )
 }
